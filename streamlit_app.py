b753cfef457cfdebcdced5c6b2871d49e4a75cbb
kaliNight <101253712+kaliNight@users.noreply.github.com>
2023-11-21 01:55:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 
 st.title("CSV Analyzer")
 
-st.write(st.secrets["OPENAI_API_KEY"])
-if st.secrets["OPENAI_API_KEY"]:
-    st.write("True")
+# st.write(st.secrets["OPENAI_API_KEY"])
+# if st.secrets["OPENAI_API_KEY"]:
+#     st.write("True")
 
 llm=OpenAI(api_token=st.secrets["OPENAI_API_KEY"])
 
@@ -26,28 +26,28 @@ else:
     st.write(":red[Please Select LLM Model]")
 
 
-# csv_file = st.file_uploader("Click to Browse CSV File",type="csv")
+csv_file = st.file_uploader("Click to Browse CSV File",type="csv")
 
-# if csv_file is not None:
-#     df=pd.read_csv(csv_file)
-#     model=SmartDataframe(df,config={"llm":llm})
-#     st.write(df.head(5))
+if csv_file is not None:
+    df=pd.read_csv(csv_file)
+    model=SmartDataframe(df,config={"llm":llm})
+    st.write(df.head(5))
 
 
-# prompt=st.text_input(label="Prompt")
-# button=st.button(label="Generate Response")
+prompt=st.text_input(label="Prompt")
+button=st.button(label="Generate Response")
 
-# if button:
-#     if prompt:
-#         with st.spinner("Generating response..."):
-#             response=model.chat(prompt)
-#             if response==None:
-#                 img= plt.imread('temp_chart.png')
-#                 st.image(img)
-#             else:
-#                 st.text_input(label="Output",value=response)
-#     else:
-#         st.write(":red[Please Write the Prompt]")
+if button:
+    if prompt:
+        with st.spinner("Generating response..."):
+            response=model.chat(prompt)
+            if response==None:
+                img= plt.imread('temp_chart.png')
+                st.image(img)
+            else:
+                st.text_input(label="Output",value=response)
+    else:
+        st.write(":red[Please Write the Prompt]")
 
 
 
