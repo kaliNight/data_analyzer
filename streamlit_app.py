f74329503874bee2a2eefb49a77f695cf494927f
kaliNight <101253712+kaliNight@users.noreply.github.com>
2023-11-21 01:53:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,43 +7,47 @@ import matplotlib.pyplot as plt
 
 st.title("CSV Analyzer")
 
-llm=OpenAI(api_token=st.secrets["OPENAI_API_KEY"])
+st.write(st.secrets["OPENAI_API_KEY"])
+if st.secrets["OPENAI_API_KEY"]:
+    st.write("True")
 
-llm_model = st.radio("LLM Model",["OpenAI", "Falcon", "Starcoder"],horizontal=True)
+# llm=OpenAI(api_token=st.secrets["OPENAI_API_KEY"])
 
-if llm_model is not None:
-    if llm_model=="OpenAI":
-        llm=OpenAI(api_token=st.secrets["OPENAI_API_KEY"])
-    elif llm_model=="Falcon":
-        llm=Falcon(api_token=st.secrets["HUGGINGFACE_API_KEY"])
-    else:
-        llm=Starcoder(api_token=st.secrets["HUGGINGFACE_API_KEY"])
-else:
-    st.write(":red[Please Select LLM Model]")
+# llm_model = st.radio("LLM Model",["OpenAI", "Falcon", "Starcoder"],horizontal=True)
 
-
-csv_file = st.file_uploader("Click to Browse CSV File",type="csv")
-
-if csv_file is not None:
-    df=pd.read_csv(csv_file)
-    model=SmartDataframe(df,config={"llm":llm})
-    st.write(df.head(5))
+# if llm_model is not None:
+#     if llm_model=="OpenAI":
+#         llm=OpenAI(api_token=st.secrets["OPENAI_API_KEY"])
+#     elif llm_model=="Falcon":
+#         llm=Falcon(api_token=st.secrets["HUGGINGFACE_API_KEY"])
+#     else:
+#         llm=Starcoder(api_token=st.secrets["HUGGINGFACE_API_KEY"])
+# else:
+#     st.write(":red[Please Select LLM Model]")
 
 
-prompt=st.text_input(label="Prompt")
-button=st.button(label="Generate Response")
+# csv_file = st.file_uploader("Click to Browse CSV File",type="csv")
 
-if button:
-    if prompt:
-        with st.spinner("Generating response..."):
-            response=model.chat(prompt)
-            if response==None:
-                img= plt.imread('temp_chart.png')
-                st.image(img)
-            else:
-                st.text_input(label="Output",value=response)
-    else:
-        st.write(":red[Please Write the Prompt]")
+# if csv_file is not None:
+#     df=pd.read_csv(csv_file)
+#     model=SmartDataframe(df,config={"llm":llm})
+#     st.write(df.head(5))
+
+
+# prompt=st.text_input(label="Prompt")
+# button=st.button(label="Generate Response")
+
+# if button:
+#     if prompt:
+#         with st.spinner("Generating response..."):
+#             response=model.chat(prompt)
+#             if response==None:
+#                 img= plt.imread('temp_chart.png')
+#                 st.image(img)
+#             else:
+#                 st.text_input(label="Output",value=response)
+#     else:
+#         st.write(":red[Please Write the Prompt]")
 
 
 
